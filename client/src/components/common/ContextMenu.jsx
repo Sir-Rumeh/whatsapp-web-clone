0ddb12683c130625163a7b2cc0d5e92bc693e4eb
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-07-14 09:22:36 +0100

    structure context menu

--- client/src/components/common/ContextMenu.jsx
+++ client/src/components/common/ContextMenu.jsx
@@ -2,9 +2,20 @@ import React, { useRef } from "react";
 
 function ContextMenu({ options, cordinates, contextMenu, setContextMenu }) {
 	const contextMenuRef = useRef(null);
+	const handleClick = (e, callback) => {};
+
 	return (
-		<div className="bg-dropdown-background fixed py-2 z-[100]" ref={contextMenuRef}>
-			ContextMenu
+		<div
+			className={`bg-dropdown-background fixed py-2 z-[100 top-[${cordinates.y}] left-[${cordinates.x}] shadow-xl`}
+			ref={contextMenuRef}
+		>
+			<ul>
+				{options.map(({ name, callback }) => (
+					<li key={name} onClick={(e) => handleClick(e, callback)}>
+						{/* <span className="text-white">{name}</span> */}
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
