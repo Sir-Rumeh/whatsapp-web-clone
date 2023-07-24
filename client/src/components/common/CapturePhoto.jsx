46e9ee37ac4cbf7f916c068a3165b1c1ada12bb4
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-07-24 17:17:30 +0100

    set up video stream for camera capture

--- client/src/components/common/CapturePhoto.jsx
+++ client/src/components/common/CapturePhoto.jsx
@@ -1,10 +1,27 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import { IoClose } from "react-icons/io5";
 
 function CapturePhoto({ hide, setImage }) {
 	const videoRef = useRef(null);
+
+	useEffect(() => {
+		let stream;
+		const startCamera = async () => {
+			stream = await navigator.mediaDevices.getUserMedia({
+				video: true,
+				audio: false,
+			});
+			videoRef.current.srcObject = stream;
+		};
+		startCamera();
+		return () => {
+			stream?.getTracks().forEach((track) => track.stop());
+		};
+	}, []);
+
+	const capturePhoto = () => {};
 	return (
-		<div className="absolute h-4/5 w-2/6 top-1/4 left-1/3 bg-gray-900 gap-3 rounded-lg pt-2 flex items-center justify-center">
+		<div className="absolute h-4/6 w-2/6 top-1/4 left-1/3 bg-gray-900 gap-3 rounded-lg pt-2 flex items-center justify-center">
 			<div className="flex flex-col gap-4 w-full">
 				<div className="pt-2 pr-2 cursor-pointer flex items-end justify-end" onClick={() => hide(false)}>
 					<IoClose className="h-10 w-10 cursor-pointer" />
@@ -12,7 +29,10 @@ function CapturePhoto({ hide, setImage }) {
 				<div className="flex justify-center">
 					<video id="video" width={"400"} autoPlay ref={videoRef}></video>
 				</div>
-				<button className="h-16 w-16 bg-white rounded-full cursor-pointer border-8 border-teal-light p-2 mb-10"></button>
+				<button
+					onClick={capturePhoto}
+					className="h-16 w-16 bg-white rounded-full cursor-pointer border-8 border-teal-light p-2 mb-10"
+				></button>
 			</div>
 		</div>
 	);
