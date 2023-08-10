375f47aea78fb5981f6c9136ecd6b14b0f47db7b
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-08-10 19:55:48 +0100

    set up message bar

--- client/src/components/common/Avatar.jsx
+++ client/src/components/common/Avatar.jsx
@@ -78,12 +78,24 @@ function Avatar({ type, image, setImage }) {
 			<div className="flex items-center justify-center">
 				{type === "sm" && (
 					<div className="relative h-10 w-10">
-						<Image src={image} alt="avatar" className="rounded-full" fill />
+						<Image
+							src={image}
+							alt="avatar"
+							className="rounded-full"
+							fill
+							sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+						/>
 					</div>
 				)}
 				{type === "lg" && (
 					<div className="relative h-14 w-14">
-						<Image src={image} alt="avatar" className="rounded-full" fill />
+						<Image
+							src={image}
+							alt="avatar"
+							className="rounded-full"
+							fill
+							sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+						/>
 					</div>
 				)}
 				{type === "xl" && (
