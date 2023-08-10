3414bd3e96d2073cc11bbe14a43976965901146e
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-08-10 21:34:19 +0100

    fix contacts list ui

--- client/src/components/Chatlist/ContactsList.jsx
+++ client/src/components/Chatlist/ContactsList.jsx
@@ -1,9 +1,9 @@
+import React, { useState, useEffect } from "react";
 import { useStateProvider } from "@/context/StateContext";
 import { reducerCases } from "@/context/constants";
 import { GET_ALL_CONTACTS } from "@/utils/ApiRoutes";
 import axios from "axios";
-import React, { useState, useEffect } from "react";
-import { BiArrowBack } from "react-icons/bi";
+import { BiArrowBack, BiSearchAlt2 } from "react-icons/bi";
 
 function ContactsList() {
 	const [{}, dispatch] = useStateProvider();
@@ -32,6 +32,23 @@ function ContactsList() {
 							dispatch({ type: reducerCases.SET_ALL_CONTACTS_PAGE });
 						}}
 					/>
+					<span>New Chat</span>
+				</div>
+			</div>
+			<div className="bg-search-input-container-background h-full flex-auto overflow-auto custom-scrollbar">
+				<div className="flex py-3 items-center gap-3 h-14">
+					<div className="bg-panel-header-background flex items-center gap-5 px-3 py-1 rounded-lg flex-grow">
+						<div>
+							<BiSearchAlt2 className="text-panel-header-icon cursor-pointer text-lg" />
+						</div>
+						<div>
+							<input
+								type="text"
+								placeholder="Search or start a new chat"
+								className="bg-transparent text-sm focus:outline-none text-white w-full"
+							/>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
