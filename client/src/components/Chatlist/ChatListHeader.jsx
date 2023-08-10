4f22c2892cf10a6043b46851eec537eb39f41541
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-08-10 21:12:55 +0100

    set up contacts list context in chat list

--- client/src/components/Chatlist/ChatListHeader.jsx
+++ client/src/components/Chatlist/ChatListHeader.jsx
@@ -2,9 +2,14 @@ import { useStateProvider } from "@/context/StateContext";
 import React from "react";
 import Avatar from "../common/Avatar";
 import { BsFillChatLeftTextFill, BsThreeDotsVertical } from "react-icons/bs";
+import { reducerCases } from "@/context/constants";
 
 function ChatListHeader() {
 	const [{ userInfo }, dispatch] = useStateProvider();
+
+	const handleAllContactsPage = () => {
+		dispatch({ type: reducerCases.SET_ALL_CONTACTS_PAGE });
+	};
 	return (
 		<div className="h-16 px-4 py-3 flex justify-between items-center">
 			<div className="cursor-pointer">
@@ -14,6 +19,7 @@ function ChatListHeader() {
 				<BsFillChatLeftTextFill
 					className="text-panel-header-icon cursor-pointer text-xl"
 					title="New Chat"
+					onClick={handleAllContactsPage}
 				/>
 				<BsThreeDotsVertical className="text-panel-header-icon cursor-pointer text-xl" title="Menu" />
 			</div>
--- client/src/components/Chatlist/ContactsList.jsx
+++ client/src/components/Chatlist/ContactsList.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { useStateProvider } from "@/context/StateContext";
+import { reducerCases } from "@/context/constants";
+import { GET_ALL_CONTACTS } from "@/utils/ApiRoutes";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { BiArrowBack } from "react-icons/bi";
 
 function ContactsList() {
-  return <div>ContactsList</div>;
+	const [{}, dispatch] = useStateProvider();
+	const [allContacts, setAllContacts] = useState([]);
+
+	useEffect(() => {
+		const getContacts = async () => {
+			try {
+				const {
+					date: { users },
+				} = await axios.get(GET_ALL_CONTACTS);
+				setAllContacts(users);
+			} catch (err) {
+				console.log(err);
+			}
+		};
+		getContacts();
+	}, []);
+	return (
+		<div className="h-ful flex flex-col">
+			<div className="h-24 flex items-end px-3 py-4">
+				<div className="flex items-center gap-12 text-white">
+					<BiArrowBack
+						className="cursor-pointer text-xl"
+						onClick={() => {
+							dispatch({ type: reducerCases.SET_ALL_CONTACTS_PAGE });
+						}}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default ContactsList;
