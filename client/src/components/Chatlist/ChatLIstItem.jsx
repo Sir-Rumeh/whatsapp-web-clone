2cb03da778438a27480cdd7a35c0b0b51d2a2a0c
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-08-11 14:03:28 +0100

    set up chat list item compomemt

--- client/src/components/Chatlist/ChatLIstItem.jsx
+++ client/src/components/Chatlist/ChatLIstItem.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Avatar from "../common/Avatar";
 
-function ChatLIstItem() {
-  return <div>ChatLIstItem</div>;
+function ChatLIstItem({ data, isContactsPage = false }) {
+	return (
+		<div className={`flex cursor-pointer items-center hover:bg-background-default-hover`}>
+			<div className="min-w-fit px-5 pt-3 pb-1">
+				<Avatar type="lg" image={data?.profilePicture} />
+			</div>
+		</div>
+	);
 }
 
 export default ChatLIstItem;
