bb587ec0b182c93d243a1c47fba8abe4e9caaba1
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-09-07 21:30:24 +0100

    contiinue to implement logic for sending audio message

--- client/src/components/common/CaptureAudio.jsx
+++ client/src/components/common/CaptureAudio.jsx
@@ -1,13 +1,53 @@
-import React from "react";
-import { FaTrash } from "react-icons/fa";
+import { useStateProvider } from "@/context/StateContext";
+import React, { useRef, useState } from "react";
+import { FaPlay, FaStop, FaTrash } from "react-icons/fa";
 
 function CaptureAudio({ hide }) {
+	const [{ userInfo, currentChatUser, socket }, dispatch] = useStateProvider;
+	const [isRecording, setIsRecording] = useState(false);
+	const [isPlaying, setIsPlaying] = useState(false);
+	const [recordedAudio, setRecordedAudio] = useState(null);
+	const [waveForm, setWaveForm] = useState(null);
+	const [recordingDuration, setRecordingDuration] = useState(0);
+	const [currentPlaybackTime, setCurrentPlaybackTime] = useState(0);
+	const [totalDuration, setTotalDuration] = useState(0);
+
+	const audioRef = useRef(null);
+	const mediaRecorderRef = useRef(null);
+	const waveFormRef = useRef(null);
+
+	const handlePlayRecording = () => {};
+	const handleStopRecording = () => {};
+
 	return (
 		<div className="flex text-2xl w-full justify-end items-center">
 			<div className="pt-1">
 				<FaTrash className="text-panel-header-icon cursor-pointer" onClick={() => hide()} />
 			</div>
-			<div className="mx-4 py-2 px-4 text-white text-lg flex gap-3 justify-center items-center"></div>
+			<div className="mx-4 py-2 px-4 text-white text-lg flex gap-3 justify-center items-center bg-search-input-container-background rounded-full drop-shadow-lg">
+				{isRecording ? (
+					<div className="text-red-500 animate-pulse w-60 text-center">
+						Recording<span>{recordingDuration}</span>
+					</div>
+				) : (
+					<div>
+						{recordedAudio && (
+							<>
+								{!isPlaying ? (
+									<FaPlay onClick={handlePlayRecording} />
+								) : (
+									<FaStop onClick={handleStopRecording} />
+								)}
+							</>
+						)}
+					</div>
+				)}
+				<div className="w-60" ref={waveFormRef} hidden={isRecording}>
+					{recordedAudio && isPlaying && <span>{formatTime(currentPlaybackTime)}</span>}
+					{recordedAudio && !isPlaying && <span>{formatTime(totalDuration)}</span>}
+					<audio ref={audioRef}></audio>
+				</div>
+			</div>
 		</div>
 	);
 }
