81c8228ccc49ed839231ac6f34a321bf99c8346c
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-09-08 07:10:46 +0100

    set up audio message recorder layout

--- client/src/components/common/CaptureAudio.jsx
+++ client/src/components/common/CaptureAudio.jsx
@@ -1,9 +1,10 @@
 import { useStateProvider } from "@/context/StateContext";
 import React, { useRef, useState } from "react";
-import { FaPlay, FaStop, FaTrash } from "react-icons/fa";
+import { FaMicrophone, FaPauseCircle, FaPlay, FaStop, FaTrash } from "react-icons/fa";
+import { MdSend } from "react-icons/md";
 
 function CaptureAudio({ hide }) {
-	const [{ userInfo, currentChatUser, socket }, dispatch] = useStateProvider;
+	const [{ userInfo, currentChatUser, socket }, dispatch] = useStateProvider();
 	const [isRecording, setIsRecording] = useState(false);
 	const [isPlaying, setIsPlaying] = useState(false);
 	const [recordedAudio, setRecordedAudio] = useState(null);
@@ -17,7 +18,10 @@ function CaptureAudio({ hide }) {
 	const waveFormRef = useRef(null);
 
 	const handlePlayRecording = () => {};
+	const handlePauseRecording = () => {};
+	const handleStartRecording = () => {};
 	const handleStopRecording = () => {};
+	const sendRecording = async () => {};
 
 	return (
 		<div className="flex text-2xl w-full justify-end items-center">
@@ -36,16 +40,30 @@ function CaptureAudio({ hide }) {
 								{!isPlaying ? (
 									<FaPlay onClick={handlePlayRecording} />
 								) : (
-									<FaStop onClick={handleStopRecording} />
+									<FaStop onClick={handlePauseRecording} />
 								)}
 							</>
 						)}
 					</div>
 				)}
-				<div className="w-60" ref={waveFormRef} hidden={isRecording}>
+				<div className="w-60 flex justify-end" ref={waveFormRef} hidden={isRecording}>
 					{recordedAudio && isPlaying && <span>{formatTime(currentPlaybackTime)}</span>}
 					{recordedAudio && !isPlaying && <span>{formatTime(totalDuration)}</span>}
-					<audio ref={audioRef}></audio>
+					<audio ref={audioRef} hidden />
+					<div className="mr-4">
+						{!isRecording ? (
+							<FaMicrophone className="text-red-500" onClick={handleStartRecording} />
+						) : (
+							<FaPauseCircle className="text-red-500" onClick={handleStopRecording} />
+						)}
+					</div>
+					<div>
+						<MdSend
+							className="text-panel-header-icon cursor-pointer mr-4"
+							title="Send"
+							onClick={sendRecording}
+						/>
+					</div>
 				</div>
 			</div>
 		</div>
