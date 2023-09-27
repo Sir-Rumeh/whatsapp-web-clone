3a877a4a2d3166fa35368149ecc3261b16819a1d
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-09-27 08:55:37 +0100

    begin configuration for zego cloud voice and video call integration

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: false,
+	env: {
+		NEXT_PUBLIC_ZEGO_APP_ID: 656124032,
+	},
 	images: {
 		domains: ["localhost"],
 	},
