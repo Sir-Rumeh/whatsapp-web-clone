8d957a7396c9b1e16ec9a01b2aeea87bbae08ea0
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-09-29 12:36:22 +0100

    set up zego cloud coonfiguration

--- client/next.config.js
+++ client/next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
 	reactStrictMode: false,
 	env: {
 		NEXT_PUBLIC_ZEGO_APP_ID: 656124032,
+		NEXT_PUBLIC_ZEGO_SERVER_ID: "212b3e9dc5c2bb23823f39f9d03bffed",
 	},
 	images: {
 		domains: ["localhost"],
