a3ffc7c1a369b61d9a69bc9f74fd4fc4a3513476
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-10-06 14:04:26 +0100

    begin implmenting logout logic

--- client/src/components/Chatlist/ChatListHeader.jsx
+++ client/src/components/Chatlist/ChatListHeader.jsx
@@ -1,15 +1,38 @@
 import { useStateProvider } from "@/context/StateContext";
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "../common/Avatar";
+import { useRouter } from "next/router";
 import { BsFillChatLeftTextFill, BsThreeDotsVertical } from "react-icons/bs";
 import { reducerCases } from "@/context/constants";
+import ContextMenu from "../common/ContextMenu";
 
 function ChatListHeader() {
+	const router = useRouter();
 	const [{ userInfo }, dispatch] = useStateProvider();
+
+	const [isContextMenuVisible, setIsContextMenuVisible] = useState(false);
+	const [contextMenuCordinates, setContextMenuCordinates] = useState({ x: 0, y: 0 });
+
+	const showContextMenu = (e) => {
+		e.preventDefault();
+		setContextMenuCordinates({ x: e.pageX - 63, y: e.pageY + 38 });
+		setIsContextMenuVisible(true);
+	};
+
+	const contextMenuOptions = [
+		{
+			name: "Logout",
+			callback: async () => {
+				setIsContextMenuVisible(false);
+				router.push("/logout");
+			},
+		},
+	];
 
 	const handleAllContactsPage = () => {
 		dispatch({ type: reducerCases.SET_ALL_CONTACTS_PAGE });
 	};
+
 	return (
 		<div className="h-16 px-4 py-3 flex justify-between items-center">
 			<div className="cursor-pointer">
@@ -21,7 +44,21 @@ function ChatListHeader() {
 					title="New Chat"
 					onClick={handleAllContactsPage}
 				/>
-				<BsThreeDotsVertical className="text-panel-header-icon cursor-pointer text-xl" title="Menu" />
+				<BsThreeDotsVertical
+					className="text-panel-header-icon cursor-pointer text-xl"
+					title="Menu"
+					id="context-opener"
+					onClick={(e) => showContextMenu(e)}
+				/>
+
+				{isContextMenuVisible ? (
+					<ContextMenu
+						options={contextMenuOptions}
+						cordinates={contextMenuCordinates}
+						contextMenu={isContextMenuVisible}
+						setContextMenu={setIsContextMenuVisible}
+					/>
+				) : null}
 			</div>
 		</div>
 	);
