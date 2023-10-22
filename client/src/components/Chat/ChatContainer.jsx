37236bf3630b065b8d4547870fc05f48f4f370ea
Nosarumen Imuentinyan <nosarumenimuentinyan@gmail.com>
2023-10-22 05:37:40 +0100

    Update ChatContainer.jsx

--- client/src/components/Chat/ChatContainer.jsx
+++ client/src/components/Chat/ChatContainer.jsx
@@ -6,33 +6,28 @@ import dynamic from "next/dynamic";
 import { GET_MESSAGES_ROUTE } from "@/utils/ApiRoutes";
 import axios from "axios";
 import { reducerCases } from "@/context/constants";
-// import Loader from "../common/Loader";
 
 const VoiceMessage = dynamic(() => import("./VoiceMessage"), { ssr: false });
 
 function ChatContainer() {
 	const [{ messages, userInfo, currentChatUser, refreshChatList }, dispatch] = useStateProvider();
 	const bottomRef = useRef(null);
-	// const [loading, setLoading] = useState(false);
 
 	useEffect(() => {
 		const getMessages = async () => {
 			try {
-				// setLoading(true);
 				const {
 					data: { messages },
 				} = await axios.get(`${GET_MESSAGES_ROUTE}/${userInfo?.id}/${currentChatUser?.id}`);
 				dispatch({ type: reducerCases.SET_MESSAGES, messages });
-				// setLoading(false);
 			} catch (err) {
-				// setLoading(false);
 				return Promise.reject(err);
 			}
 		};
 		if (currentChatUser?.id) {
 			getMessages();
 		}
-	}, [currentChatUser]);
+	}, [currentChatUser, refreshChatList]);
 
 	useEffect(() => {
 		if (messages.length) {
@@ -48,8 +43,7 @@ function ChatContainer() {
 			<div className="bg-chat-background bg-fixed h-full w-full opacity-5 fixed left-0 top-0 z-0"></div>
 			<div className="m-6 relative bottom-0 left-0">
 				<div className="flex w-full">
-				
-						<div className="flex flex-col justify-end w-full gap-[6px] overflow-auto relative">
+					<div className="flex flex-col justify-end w-full gap-[6px] overflow-auto relative">
 							{messages?.map((message) => (
 								<div
 									key={message.id}
@@ -64,8 +58,7 @@ function ChatContainer() {
 									{message.type === "audio" && <VoiceMessage message={message} />}
 								</div>
 							))}
-						</div>
-					
+					</div>
 					<div ref={bottomRef} />
 				</div>
 			</div>
