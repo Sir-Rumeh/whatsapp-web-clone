21a8fef3ea3a84430c35711b831a0e5251fffb95
Nosarumen Imuentinyan <nosarumenimuentinyan@gmail.com>
2023-10-22 05:45:24 +0100

    Update List.jsx

--- client/src/components/Chatlist/List.jsx
+++ client/src/components/Chatlist/List.jsx
@@ -2,68 +2,74 @@ import { useStateProvider } from "@/context/StateContext";
 import { reducerCases } from "@/context/constants";
 import { GET_INITIAL_CONTACTS_ROUTE } from "@/utils/ApiRoutes";
 import axios from "axios";
-// import Loader from "../common/Loader";
-import React, { useEffect } from "react";
+import Loader from "../common/Loader";
+import React, { useState, useEffect } from "react";
 import ChatLIstItem from "./ChatLIstItem";
 import { useRouter } from "next/router";
 
 
 function List() {
 	const router = useRouter();
-	const [{ userInfo, currentChatUser, userContacts, filteredContacts }, dispatch] =
+	const [{ userInfo, currentChatUser, userContacts, filteredContacts, refreshChatList }, dispatch] =
 		useStateProvider();
-	// const [loading, setLoading] = useState(false);
+	const [loading, setLoading] = useState(false);
 	
 	
 
 	useEffect(() => {
 		const getContacts = async () => {
 			try {
-				// setLoading(true);
+				setLoading(true);
 				const {
 					data: { users, onlineUsers },
 				} = await axios.get(`${GET_INITIAL_CONTACTS_ROUTE}/${userInfo?.id}`);
 				dispatch({ type: reducerCases.SET_USER_CONTACTS, userContacts: users });
 				dispatch({ type: reducerCases.SET_ONLINE_USERS, onlineUsers });
-				// setLoading(false);
+				setLoading(false);
 			} catch (err) {
-				// setLoading(false);
+				setLoading(false);
 				return Promise.reject(err);
 			}
 		};
 		if (userInfo?.id) {
 			getContacts();
 		}
-	}, []);
+	}, [refreshChatList]);
 
 	return (
 		<>
-			<div className="bg-search-input-container-background flex-auto overflow-auto max-h-full custom-scrollbar px-1 relative">
-					{filteredContacts && filteredContacts.length > 0
-						? filteredContacts?.map((contact) => (
-								<div className="" key={contact.id}>
-									<ChatLIstItem data={contact} />
-								</div>
-						  ))
-						: userContacts?.map((contact) => (
-								<div
-									className={`${
-										currentChatUser?.id === contact.id ? " bg-[#2e3d4b] rounded-sm" : ""
-									}`}
-									key={contact.id}
-								>
-									<ChatLIstItem data={contact} />
-								</div>
-						  ))}
-					<div className="absolute bottom-3 w-full px-3">
-						<button
-							className="w-full px-3 py-3 rounded-md font-bold bg-outgoing-background text-white text-xl"
-							onClick={() => router.push("/aichat")}
-						>
-							Chat with AI Bot
-						</button>
-					</div>
-			</div>
+			{loading ? (
+				<div className="bg-search-input-container-background flex-auto overflow-auto max-h-full custom-scrollbar px-1">
+					<Loader loading={loading} />
+				</div>
+			) : (
+				<div className="bg-search-input-container-background flex-auto overflow-auto max-h-full custom-scrollbar px-1 relative">
+						{filteredContacts && filteredContacts.length > 0
+							? filteredContacts?.map((contact) => (
+									<div className="" key={contact.id}>
+										<ChatLIstItem data={contact} />
+									</div>
+							  ))
+							: userContacts?.map((contact) => (
+									<div
+										className={`${
+											currentChatUser?.id === contact.id ? " bg-[#2e3d4b] rounded-sm" : ""
+										}`}
+										key={contact.id}
+									>
+										<ChatLIstItem data={contact} />
+									</div>
+							  ))}
+						<div className="absolute bottom-3 w-full px-3">
+							<button
+								className="w-full px-3 py-3 rounded-md font-bold bg-outgoing-background text-white text-xl"
+								onClick={() => router.push("/aichat")}
+							>
+								Chat with AI Bot
+							</button>
+						</div>
+				</div>
+			)}
 		</>
 	);
 }
