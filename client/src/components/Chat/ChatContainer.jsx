141456ad9d77b87c8b15806cec85db361957384b
Nosarumen Imuentinyan <nosarumenimuentinyan@gmail.com>
2023-10-22 05:25:23 +0100

    Update ChatContainer.jsx

--- client/src/components/Chat/ChatContainer.jsx
+++ client/src/components/Chat/ChatContainer.jsx
@@ -1,38 +1,38 @@
 import { useStateProvider } from "@/context/StateContext";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import ImageMessage from "./ImageMessage";
 import TextMessage from "./TextMessage";
 import dynamic from "next/dynamic";
 import { GET_MESSAGES_ROUTE } from "@/utils/ApiRoutes";
 import axios from "axios";
 import { reducerCases } from "@/context/constants";
-import Loader from "../common/Loader";
+// import Loader from "../common/Loader";
 
 const VoiceMessage = dynamic(() => import("./VoiceMessage"), { ssr: false });
 
 function ChatContainer() {
 	const [{ messages, userInfo, currentChatUser, refreshChatList }, dispatch] = useStateProvider();
 	const bottomRef = useRef(null);
-	const [loading, setLoading] = useState(false);
+	// const [loading, setLoading] = useState(false);
 
 	useEffect(() => {
 		const getMessages = async () => {
 			try {
-				setLoading(true);
+				// setLoading(true);
 				const {
 					data: { messages },
 				} = await axios.get(`${GET_MESSAGES_ROUTE}/${userInfo?.id}/${currentChatUser?.id}`);
 				dispatch({ type: reducerCases.SET_MESSAGES, messages });
-				setLoading(false);
+				// setLoading(false);
 			} catch (err) {
-				setLoading(false);
+				// setLoading(false);
 				return Promise.reject(err);
 			}
 		};
 		if (currentChatUser?.id) {
 			getMessages();
 		}
-	}, [currentChatUser, refreshChatList]);
+	}, [currentChatUser]);
 
 	useEffect(() => {
 		if (messages.length) {
@@ -48,11 +48,7 @@ function ChatContainer() {
 			<div className="bg-chat-background bg-fixed h-full w-full opacity-5 fixed left-0 top-0 z-0"></div>
 			<div className="m-6 relative bottom-0 left-0">
 				<div className="flex w-full">
-					{loading ? (
-						<div className="flex items-center justify-center h-[80vh] w-full ">
-							<Loader loading={loading} />
-						</div>
-					) : (
+				
 						<div className="flex flex-col justify-end w-full gap-[6px] overflow-auto relative">
 							{messages?.map((message) => (
 								<div
@@ -69,7 +65,7 @@ function ChatContainer() {
 								</div>
 							))}
 						</div>
-					)}
+					
 					<div ref={bottomRef} />
 				</div>
 			</div>
