e4e294fe417269c0193b13ad185176de9e32992d
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-10-23 12:57:35 +0100

    fix onboarding page responsiveness

--- client/src/pages/onboarding.jsx
+++ client/src/pages/onboarding.jsx
@@ -78,7 +78,7 @@ function onboarding() {
 				</div>
 			) : (
 				<div className="bg-panel-header-background h-screen w-screen text-white flex flex-col items-center justify-center">
-					<div className="flex items-center justify-center gap-2">
+					<div className="hidden md:flex items-center justify-center gap-2">
 						<Image
 							src="/whatsapp.gif"
 							alt="whatsapp"
@@ -86,11 +86,17 @@ function onboarding() {
 							width={300}
 							priority
 							sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+							className="scale-75 md:scale-100"
 						/>
-						<span className="text-7xl">Whatsapp</span>
+						<span className="flex justify-center w-full text-center text-5xl md:text-7xl">
+							Whatsapp
+						</span>
 					</div>
 					<h2 className="text-2xl">Create your profile</h2>
-					<div className="flex gap-6 mt-6">
+					<div className="md:flex gap-6 mt-6">
+						<div className="flex md:hidden">
+							<Avatar type="xl" image={image} setImage={setImage} />
+						</div>
 						<div className="flex flex-col items-center justify-center mt-5 gap-6">
 							<div>
 								<Input name="Display Name" state={name} setState={setName} label />
@@ -101,14 +107,14 @@ function onboarding() {
 							<Input name="About" state={about} setState={setAbout} label />
 							<div className="flex items-center justify-center">
 								<button
-									className="flex items-center justify-center gap-7 bg-search-input-container-background p-5 rounded-lg"
+									className="flex items-center justify-center gap-7 bg-search-input-container-background p-3 md:p-5 rounded-lg"
 									onClick={onboardUserHandler}
 								>
 									Create Profile
 								</button>
 							</div>
 						</div>
-						<div>
+						<div className="hidden md:flex">
 							<Avatar type="xl" image={image} setImage={setImage} />
 						</div>
 					</div>
