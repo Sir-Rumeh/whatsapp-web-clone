c9f2d98f286e97cc99acacc0588f7eaa58db7bcf
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-10-26 08:01:48 +0100

    clean up

--- client/src/pages/_document.jsx
+++ client/src/pages/_document.jsx
@@ -5,7 +5,6 @@ export default function Document() {
 		<Html lang="en">
 			<Head>
 				<script src="https://unpkg.com/wavesurfer.js" />
-				{/* <script src="path-to-waveform-js/waveplayer.js"></script> */}
 			</Head>
 			<body>
 				<Main />
